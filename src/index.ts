c949a9b058301bf3ab731940dcb0280c65287dba
molarmanful <ben@benpa.ng>
2025-06-15 13:02:27 -0500

    feat: add stylistic config

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import eslint from '@eslint/js'
 import { composer } from 'eslint-flat-config-utils'
 import tseslint from 'typescript-eslint'
+import stylistic from './configs/stylistic'
 
-export const molarmanfulConfig = () => composer(
-  tseslint.config(
-    eslint.configs.recommended,
-    tseslint.configs.recommended,
-  ),
+interface Options {
+
+}
+
+export const molarmanfulConfig = (_: Options) => composer(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  stylistic,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
 )
