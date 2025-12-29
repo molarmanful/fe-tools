154b47f8d2acfa03c791d5fc781f1526eec955f5
molarmanful <ben@benpa.ng>
2025-12-28 22:27:08 -0500

    fix: typechecking

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import unicorn from './configs/unicorn'
 import unocss from './configs/unocss'
 import { Opts } from './lib/opts'
 
-export const molarmanfulLint = (opts: Opts = {}) =>
+export const molarmanfulLint = (opts: Opts = {}): ReturnType<typeof composer> =>
   composer(
     gitignore({ strict: false }),
     js,
